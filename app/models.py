5a8c6f28202cb843c5b6052eae1975cc291f868e
GideonKipkemoi <rotichgideon760@gmail.com>
2023-09-22 14:33:01 +0300

    Added a date validation on the models

--- app/models.py
+++ app/models.py
@@ -1,26 +1,41 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 Universities = (
-    ("...........","..........."),
-    ("moi university","moi university"),
+    ("Learning institution","Learning institution"),
+    ("Moi university","Moi university"),
+    ("kenyatta university","Kenyatta university"),
+    ("University of Nairobi","University of Nairobi"),
+    ("Chuka university","Chuka university"),
+    ("Egerton university","Egerton university"),
+    ("University of Eldoret","University of Eldoret"),
     
 )
+
+def validate_post_date(post_date):
+    if post_date<timezone.now():
+        raise ValidationError("you cannot pick a past date")
 
 class Post(models.Model):
     position = models.CharField(max_length=30)
     company = models.CharField(max_length=30)
     description = models.TextField()
-    start_date = models.DateTimeField()
+    start_date = models.DateTimeField(validators=[validate_post_date])
+    
     
     def __str__(self):
         return self.position
+    
+def validate_apply_date(apply_date):
+    if apply_date<Post.start_date:
+        raise ValidationError("you cannot pick a past date")
 class Apply(models.Model):
     name = models.CharField(max_length=50)
     learning_institution = models.CharField(
         max_length=100,
         choices=Universities,
-        default="..........."
+        default="Learning institution"
     )
     applied_position = models.ForeignKey(Post, on_delete=models.CASCADE)
     expected_start_date = models.DateTimeField()
